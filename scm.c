6c4eb6593a72c58bc6e719328665b6513cd890f6
José Aurélio Carvalho <joseaurelio.99.jaec@gmail.com>
2025-02-24 11:43:34 -0300

    Added divide

--- scm.c
+++ scm.c
@@ -8,6 +8,14 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void divide(int a, int b){
+    if(b == 0){
+        printf("o numero nao pode ser dividido por zero");
+    }else{
+        printf("%f", (a/b));
+    }
+}
+
 int main() {
     printf("Olá, Estudante!\n");
     soma(5, 3);
